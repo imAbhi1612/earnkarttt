5232c33e7df02f2e541e831ad6a0daa59dfa35ff
imAbhi1612 <159281816+imAbhi1612@users.noreply.github.com>
2024-03-19 01:00:34 +0530

    Update useLogout.ts

--- client/src/hooks/useLogout.ts
+++ client/src/hooks/useLogout.ts
@@ -15,6 +15,7 @@ export default function useLogout() {
         withCredentials: true,
       });
       setAuth(initialAuthState);
+      localStorage.removeItem('userId');
       removeCookie('userDetails');
       toast.success('Logged out successfully');
       navigate('/login');
