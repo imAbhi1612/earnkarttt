7d7086d5c893a8e89d7dae642b5f5fbb0d7e31b9
imAbhi1612 <159281816+imAbhi1612@users.noreply.github.com>
2024-04-12 10:48:52 +0530

    Update CreateForm.tsx

--- client/src/pages/CreateForm.tsx
+++ client/src/pages/CreateForm.tsx
@@ -1,18 +1,10 @@
 import { useLocation, useNavigate, useParams } from 'react-router-dom';
-import { DndContext, DragOverlay, useSensor, useSensors } from '@dnd-kit/core';
-import { restrictToWindowEdges } from '@dnd-kit/modifiers';
-import { sortableKeyboardCoordinates } from '@dnd-kit/sortable';
-import { KeyboardSensor, PointerSensor } from '../lib/dndKitSensors';
 import FormElements from '../components/create-form/FormElements';
-import {
-  FormElementButton,
-  FormElementButtonProps,
-} from '../components/create-form/DraggableButton';
 import { useEffect, useState } from 'react';
 import FormPlayground from '../components/create-form/FormPlayground';
 import Input from '../components/ui/Input';
 import { Button } from '../components/ui/Button';
-import { EyeIcon, HammerIcon, LockIcon } from 'lucide-react';
+import { EyeIcon, HammerIcon } from 'lucide-react';
 import {
   AlertDialog,
   AlertDialogAction,
@@ -49,9 +41,19 @@ export default function CreateForm({ formType = 'add', form }: Props) {
 
   const [formName, setFormName] = useState(form?.name ?? '');
   const [partnerLink, setPartnerLink] = useState(form?.partnerLink ?? '');
-  const [activeButton, setActiveButton] =
-    useState<FormElementButtonProps | null>(null);
-  const [isDropped, setIsDropped] = useState(false);
+
+  // const addFormElement = useFormPlaygroundStore(state => state.addFormElement);
+  const removeAllFormElements = useFormPlaygroundStore(
+    state => state.removeAllFormElements,
+  );
+  const formElements = useFormPlaygroundStore(state => state.formElements);
+
+  useEffect(() => {
+    return () => {
+      if (formType === 'edit') removeAllFormElements();
+    };
+  }, [removeAllFormElements, formType]);
+
   const handleAddUniqueCode = () => {
     const textarea = document.getElementById(
       'partnerLinkTextarea',
@@ -72,25 +74,6 @@ export default function CreateForm({ formType = 'add', form }: Props) {
       textarea.setSelectionRange(newCursorPos, newCursorPos);
     }
   };
-
-  const addFormElement = useFormPlaygroundStore(state => state.addFormElement);
-  const removeAllFormElements = useFormPlaygroundStore(
-    state => state.removeAllFormElements,
-  );
-  const formElements = useFormPlaygroundStore(state => state.formElements);
-
-  useEffect(() => {
-    return () => {
-      if (formType === 'edit') removeAllFormElements();
-    };
-  }, [removeAllFormElements, formType]);
-
-  const sensors = useSensors(
-    useSensor(PointerSensor),
-    useSensor(KeyboardSensor, {
-      coordinateGetter: sortableKeyboardCoordinates,
-    }),
-  );
 
   const axiosPrivate = useAxiosPrivate();
   const { mutate, isPending } = useMutation({
@@ -120,150 +103,119 @@ export default function CreateForm({ formType = 'add', form }: Props) {
   });
 
   return (
-    <DndContext
-      sensors={sensors}
-      onDragStart={e => {
-        setActiveButton(e.active.data.current?.element);
-        setIsDropped(false);
-      }}
-      onDragCancel={() => {
-        setActiveButton(null);
-        setIsDropped(false);
-      }}
-      onDragEnd={({ over, active }) => {
-        setActiveButton(null);
-        if (!over) return;
-        addFormElement(
-          active.data.current?.element.text as string,
-          active.id as string,
-        );
-        setIsDropped(true);
-      }}
-    >
-      <div className="flex flex-col gap-5 md:flex-row md:gap-12">
-        <FormElements isUpdate={formType === 'edit'} />
-        <form
-          className="flex flex-grow flex-col"
-          onSubmit={e => {
-            e.preventDefault();
-            if (formElements.length === 0) {
-              toast.error('Form is empty!');
-              return;
-            }
-            mutate();
-          }}
-        >
-          <section className="mb-3">
-            <div className="flex flex-col items-center gap-3 md:flex-row md:gap-4">
-              <label className="font-medium">Form Name:</label>
-              <Input
-                required
-                placeholder="Enter form name"
-                value={formName}
-                onChange={e => setFormName(e.target.value)}
-              />
-            </div>
-            <div className="mt-3 flex flex-col items-center gap-3 md:mt-0 md:flex-row md:gap-4">
-              <label className="font-medium">Partner Link:</label>
-              <textarea
-                id="partnerLinkTextarea"
-                required
-                placeholder="Enter Partner Link"
-                value={partnerLink}
-                onChange={e => setPartnerLink(e.target.value)}
-                className="flex min-h-[60px] w-full rounded-md border bg-transparent px-3 py-2 text-sm shadow-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
-              />
-            </div>
-            <button
-              type="button"
-              className="hover:bg-primary-dark ml-2 mt-4 rounded-md bg-primary px-2 py-1 text-sm font-medium text-white shadow-sm transition-colors focus:outline-none focus:ring-2 focus:ring-primary"
-              onClick={handleAddUniqueCode}
+    <div className="flex flex-col gap-5 md:flex-row md:gap-12">
+      <FormElements isUpdate={formType === 'edit'} />
+      <form
+        className="flex flex-grow flex-col"
+        onSubmit={e => {
+          e.preventDefault();
+          if (formElements.length === 0) {
+            toast.error('Form is empty!');
+            return;
+          }
+          mutate();
+        }}
+      >
+        <section className="mb-3">
+          <div className="flex flex-col items-center gap-3 md:flex-row md:gap-4">
+            <label className="font-medium">Form Name:</label>
+            <Input
+              required
+              placeholder="Enter form name"
+              value={formName}
+              onChange={e => setFormName(e.target.value)}
+            />
+          </div>
+          <div className="mt-3 flex flex-col items-center gap-3 md:mt-0 md:flex-row md:gap-4">
+            <label className="font-medium">Partner Link:</label>
+            <textarea
+              id="partnerLinkTextarea"
+              required
+              placeholder="Enter Partner Link"
+              value={partnerLink}
+              onChange={e => setPartnerLink(e.target.value)}
+              className="flex min-h-[60px] w-full rounded-md border bg-transparent px-3 py-2 text-sm shadow-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
+            />
+          </div>
+          <button
+            type="button"
+            className="hover:bg-primary-dark ml-2 mt-4 rounded-md bg-primary px-2 py-1 text-sm font-medium text-white shadow-sm transition-colors focus:outline-none focus:ring-2 focus:ring-primary"
+            onClick={handleAddUniqueCode}
+          >
+            Add Unique Code
+          </button>
+        </section>
+        <section className="mt-3 md:mt-5">
+          <div className="mb-2 flex items-center gap-4 text-sm font-medium">
+            <div
+              className={`flex items-center gap-2 transition-colors ${
+                isPreview ? '' : 'text-primary'
+              }`}
             >
-              Add Unique Code
-            </button>
-          </section>
-          <section className="mt-3 md:mt-5">
-            <div className="mb-2 flex items-center gap-4 text-sm font-medium">
-              <div
-                className={`flex items-center gap-2 transition-colors ${
-                  isPreview ? '' : 'text-primary'
-                }`}
-              >
-                <HammerIcon className="h-5 w-5" />
-                <span>Builder</span>
-              </div>
-              <Switch
-                className="data-[state=unchecked]:bg-primary"
-                checked={isPreview}
-                onCheckedChange={setIsPreview}
-              />
-              <div
-                className={`flex items-center gap-2 transition-colors ${
-                  isPreview ? 'text-primary' : ''
-                }`}
-              >
-                <EyeIcon className="h-5 w-5" />
-                <span>Preview</span>
-              </div>
+              <HammerIcon className="h-5 w-5" />
+              <span>Builder</span>
             </div>
-            {isPreview ? (
-              <FormPreview />
-            ) : (
-              <FormPlayground
-                isDropped={isDropped}
-                resetIsDropped={() => setIsDropped(false)}
-                isUpdate={formType === 'edit'}
-              />
-            )}
-          </section>
-          <section className="mt-5 flex items-center gap-5 md:self-end">
-            {isDemo && <DemoInfoCard />}
-            {form && (
-              <Button onClick={() => navigate('/my-forms')} variant="outline">
-                Cancel
-              </Button>
-            )}
-            {formElements.length !== 0 && (
-              <AlertDialog>
-                <AlertDialogTrigger asChild>
-                  <Button type="button" variant="destructive">
-                    Clear Form
-                  </Button>
-                </AlertDialogTrigger>
-                <AlertDialogContent>
-                  <AlertDialogHeader>
-                    <AlertDialogTitle>Clear Form?</AlertDialogTitle>
-                    <AlertDialogDescription>
-                      Are you sure you want to clear the form? This action is
-                      irreversible and will permanently remove all the progress
-                      in the current form.
-                    </AlertDialogDescription>
-                  </AlertDialogHeader>
-                  <AlertDialogFooter className="sm:space-x-4">
-                    <AlertDialogAction onClick={removeAllFormElements}>
-                      Yes, clear form
-                    </AlertDialogAction>
-                    <AlertDialogCancel>Cancel</AlertDialogCancel>
-                  </AlertDialogFooter>
-                </AlertDialogContent>
-              </AlertDialog>
-            )}
-            <Button
-              disabled={isDemo}
-              isLoading={isPending}
-              className={isDemo ? 'gap-2.5' : ''}
+            <Switch
+              className="data-[state=unchecked]:bg-primary"
+              checked={isPreview}
+              onCheckedChange={setIsPreview}
+            />
+            <div
+              className={`flex items-center gap-2 transition-colors ${
+                isPreview ? 'text-primary' : ''
+              }`}
             >
-              {isDemo && <LockIcon className="h-[18px] w-[18px]" />}
-              <span>{form ? 'Update Form' : 'Create Form'}</span>
+              <EyeIcon className="h-5 w-5" />
+              <span>Preview</span>
+            </div>
+          </div>
+          {isPreview ? (
+            <FormPreview />
+          ) : (
+            <FormPlayground isUpdate={formType === 'edit'} />
+          )}
+        </section>
+        <section className="mt-5 flex items-center gap-5 md:self-end">
+          {isDemo && <DemoInfoCard />}
+          {form && (
+            <Button onClick={() => navigate('/my-forms')} variant="outline">
+              Cancel
             </Button>
-          </section>
-        </form>
-      </div>
-      <DragOverlay modifiers={[restrictToWindowEdges]}>
-        {activeButton ? (
-          <FormElementButton className="cursor-grabbing" {...activeButton} />
-        ) : null}
-      </DragOverlay>
-    </DndContext>
+          )}
+          {formElements.length !== 0 && (
+            <AlertDialog>
+              <AlertDialogTrigger asChild>
+                <Button type="button" variant="destructive">
+                  Clear Form
+                </Button>
+              </AlertDialogTrigger>
+              <AlertDialogContent>
+                <AlertDialogHeader>
+                  <AlertDialogTitle>Clear Form?</AlertDialogTitle>
+                  <AlertDialogDescription>
+                    Are you sure you want to clear the form? This action is
+                    irreversible and will permanently remove all the progress in
+                    the current form.
+                  </AlertDialogDescription>
+                </AlertDialogHeader>
+                <AlertDialogFooter className="sm:space-x-4">
+                  <AlertDialogAction onClick={removeAllFormElements}>
+                    Yes, clear form
+                  </AlertDialogAction>
+                  <AlertDialogCancel>Cancel</AlertDialogCancel>
+                </AlertDialogFooter>
+              </AlertDialogContent>
+            </AlertDialog>
+          )}
+          <Button
+            disabled={isDemo}
+            isLoading={isPending}
+            className={isDemo ? 'gap-2.5' : ''}
+          >
+            <span>{form ? 'Update Form' : 'Create Form'}</span>
+          </Button>
+        </section>
+      </form>
+    </div>
   );
 }
