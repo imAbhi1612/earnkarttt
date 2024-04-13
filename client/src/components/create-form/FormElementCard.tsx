e085031ba4a955afaa0f895b2d09d829f62260ce
imAbhi1612 <159281816+imAbhi1612@users.noreply.github.com>
2024-04-13 11:59:45 +0530

    Update FormElementCard.tsx

--- client/src/components/create-form/FormElementCard.tsx
+++ client/src/components/create-form/FormElementCard.tsx
@@ -45,7 +45,7 @@ export default function FormElementCard({
 
   return (
     <article
-      className={`relative flex gap-2 rounded-md bg-white py-3 shadow ${
+      className={`relative flex gap-2 rounded-md bg-white py-3 ${
         isView ? 'px-5' : 'pl-2 pr-4'
       }`}
     >
