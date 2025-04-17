f8a779a8ac57806f30158984a3a7fb3fcbe379f8
imAbhi1612 <159281816+imAbhi1612@users.noreply.github.com>
2025-04-18 02:14:10 +0530

    Update FormElementCard.tsx

--- client/src/components/create-form/FormElementCard.tsx
+++ client/src/components/create-form/FormElementCard.tsx
@@ -62,6 +62,7 @@ export default function FormElementCard({
               <Switch
                 checked={field?.value}
                 onCheckedChange={field?.onChange}
+                required={field ? required : false}
               />
             ) : type === 'checkbox' ? (
               <Checkbox
@@ -152,6 +153,7 @@ export default function FormElementCard({
               <li key={value} className="flex items-center gap-3">
                 <Checkbox
                   id={value}
+                  required={field ? required : false}
                   checked={field?.value?.includes(label) ?? false}
                   onCheckedChange={checked => {
                     if (checked) field?.onChange([...field.value, label]);
